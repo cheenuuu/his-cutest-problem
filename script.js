40dc364ef9a94f0d0d332ddbbc3ad68d60a9c992
vaishnaviiii <triyashagupta9@gmail.com>
2026-02-14 16:14:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 function sayYes() {
+  const firstPage = document.getElementById("firstPage");
+  const secondPage = document.getElementById("secondPage");
+  const img = document.getElementById("loveImg");
   const song = document.getElementById("loveSong");
+
+  // Start music
   song.play();
 
+  // Hide first page
+  firstPage.style.display = "none";
+
+  // Show second page
+  secondPage.style.display = "block";
+
+  // Animate image
   setTimeout(() => {
-    window.location.href = "second.html";
-  }, 2000);
+    img.classList.add("show");
+  }, 500);
 }
-
-
